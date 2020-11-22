921d438f534fc1c89e0efd3063a3e6f3794a5c50
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2020-11-22 20:59:30 +0000

    removed console logs

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ function testPalindrome(event){
   const text = input.value.split(' ').join('').toLowerCase();
   const textReverse = text.split('').reverse().join('');
 
-  console.log(text)
-  console.log(textReverse)
-
   if(text === ""){
     alert('Please add some text');
     }else if(text === textReverse) {
@@ -22,11 +19,13 @@ function testPalindrome(event){
 }
 
 function result(outcome){
+
+  const message = document.querySelector('.results')
   
   if(outcome === true){
-    document.querySelector('.results').innerText =`${input.value} is a Palindrome`;
+    message.innerText =`${input.value} is a Palindrome`;
   } else {
-    document.querySelector('.results').innerText =`${input.value} is NOT a Palindrome!`;
+    message.innerText =`${input.value} is NOT a Palindrome!`;
   }
 }
 
