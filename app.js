8866382163cc69cb0aa7a0497731c68062c92924
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2020-11-22 20:55:37 +0000

    bug fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ input.addEventListener('click', resetForm);
 
 function testPalindrome(event){
   const text = input.value.split(' ').join('').toLowerCase();
-  const textReverse = text.split(' ').reverse().join('');
+  const textReverse = text.split('').reverse().join('');
 
   console.log(text)
   console.log(textReverse)
@@ -24,9 +24,9 @@ function testPalindrome(event){
 function result(outcome){
   
   if(outcome === true){
-    document.querySelector('.results').innerText ='That is a Palindrome!';
+    document.querySelector('.results').innerText =`${input.value} is a Palindrome`;
   } else {
-    document.querySelector('.results').innerText ='That is NOT a Palindrome!';
+    document.querySelector('.results').innerText =`${input.value} is NOT a Palindrome!`;
   }
 }
 
